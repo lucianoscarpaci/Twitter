e79e1dcd202a0e84509ee0b16bb92fd287d32d1c
Luciano Scarpaci <Scarpacii.luciano@gmail.com>
2022-09-15 00:10:38 -0400

    User can log in - completed

--- twrrrrrclient/Twitter/TwitterAPICaller.swift
+++ twrrrrrclient/Twitter/TwitterAPICaller.swift
@@ -10,7 +10,7 @@ import UIKit
 import BDBOAuth1Manager
 
 class TwitterAPICaller: BDBOAuth1SessionManager {    
-    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "5lUJuO5AUpPUCez4ewYDFrtgh", consumerSecret: "s5ynGqXzstUZwFPxVyMDkYh197qvHOcVM3kwv1o2TKhS1avCdS")
+    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "kLPUhmOhF3W8RQcDGzmlPu6ls", consumerSecret: "UQYcV42OkSF4jPcsZ3qb6Ngk2RLt6otV1Q6Jx3j1OCIcX0YKjI")
     var loginSuccess: (() -> ())?
     var loginFailure: ((Error) -> ())?
     
