364fba4e8cb2f9d68b7e6d92753f2a69c47a6494
Luciano Scarpaci <Scarpacii.luciano@gmail.com>
2022-09-18 21:41:11 -0400

    User can favorite a tweet

--- twrrrrrclient/Twitter/TweetCellTableViewCell.swift
+++ twrrrrrclient/Twitter/TweetCellTableViewCell.swift
@@ -18,12 +18,41 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBOutlet weak var retweetButton: UIButton!
     @IBOutlet weak var favButton: UIButton!
     
+    var favorited:Bool = false
+    var tweetId:Int = -1
+    
     @IBAction func favoriteTweet(_ sender: Any) {
+        let toBeFavorited = !favorited
+        if (toBeFavorited) {
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(true)
+            }, failure: { (error) in
+                print("Favorite did not succceed: \(error)")
+            })
+        } else {
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(false)
+            }, failure: { (error) in
+                print("Unfavorite did not succeed: \(error)")
+            })
+        }
+    
     }
     
     @IBAction func doRetweet(_ sender: Any) {
     }
     
+
+    
+    func setFavorite(_ isFavorited:Bool) {
+        favorited = isFavorited
+        if (favorited) {
+            favButton.setImage(UIImage(named: "favor-icon-red"), for: UIControl.State.normal)
+        }
+        else {
+            favButton.setImage(UIImage(named: "favor-icon-1"), for: UIControl.State.normal)
+        }
+    }
     
     
     override func awakeFromNib() {
